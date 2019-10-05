ee4c25030b1bc34dede448dc412e45780b211682
Celso Inácio <celsoinacio_@hotmail.com>
2019-10-05 17:56:58 -0300

    Add netos-house to subsite

---
+++ netos-house/precache-manifest.01c2b5330a7ba0c6f9d29e7b53c4d0aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a7375fdc6f2784ae5ceb5cc5222115d",
+    "url": "/static/media/slide1.7a7375fd.jpg"
+  },
+  {
+    "revision": "dda4a659f3361d19d378",
+    "url": "/static/css/main.22177f09.chunk.css"
+  },
+  {
+    "revision": "b340dc9ad8634415ce92",
+    "url": "/static/js/1.b340dc9a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f42d83b23582f35d5993cb7f81c32e13",
+    "url": "/static/media/salao.f42d83b2.jpg"
+  },
+  {
+    "revision": "b26cbcfe69f167da9ccd5fe591c71050",
+    "url": "/static/media/logo.b26cbcfe.png"
+  },
+  {
+    "revision": "dda4a659f3361d19d378",
+    "url": "/static/js/main.dda4a659.chunk.js"
+  },
+  {
+    "revision": "baacec7b312e245f6d46379e1c945aa2",
+    "url": "/static/media/slide2.baacec7b.jpg"
+  },
+  {
+    "revision": "5001c12b3347093baaf9ee656434306b",
+    "url": "/static/media/slide3.5001c12b.jpg"
+  },
+  {
+    "revision": "8c1861973d72586208ec3406d74c4b02",
+    "url": "/static/media/slide4.8c186197.jpg"
+  },
+  {
+    "revision": "9efadac1c5fa86feb9cbe38cc2431933",
+    "url": "/static/media/slide5.9efadac1.jpg"
+  },
+  {
+    "revision": "5f0c0f5d27344687180f65c4b7d7ff18",
+    "url": "/static/media/video.5f0c0f5d.mp4"
+  },
+  {
+    "revision": "f25f0395fd409d1c100e7380b2be2469",
+    "url": "/index.html"
+  }
+];
